cfc2feed37f1dd41d3746b848d5ccca9433c32bc
root <412767624@qq.com>
2019-06-03 15:32:05 +0800

    jdbc连接数据库

---
+++ jdbc/src/main/java/com/Test.java
@@ -0,0 +1,46 @@
+package com;
+import java.sql.*;
+public class Test {
+    public static void main(String[] args) {
+        int a = 400, b = 60, c = a * b;
+        Connection conn = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/text?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true", "root", "123456");
+            conn.setAutoCommit(false);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println ("捕获异常===-----------=" );
+        }
+        String sql = "INSERT INTO student(name,age) VALUES (?,?)";
+        long startTime = System.currentTimeMillis();
+
+        try {
+            StringBuilder sqls = new StringBuilder();
+
+            conn.setAutoCommit(false);
+
+            PreparedStatement pst = conn.prepareStatement(sql);
+
+            for (int x = 1; x <= a; x++) {
+                for (int y = 1; y <= b; y++) {
+                    pst.setString(1, "张三");
+                    pst.setString(2, "56");
+                    pst.addBatch();
+                }
+                pst.executeBatch();
+                conn.commit();
+            }
+            pst.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println ("捕获异常====" );
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("插入" + c + "条数据, " + "运行时间：" + (endTime - startTime)/1000 + "秒");
+    }
+}
+
